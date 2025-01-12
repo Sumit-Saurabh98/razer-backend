6b4eede5d496c38f2e8accc7234d6cff4c53b274
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2025-01-12 21:48:27 +0530

    done

--- src/utils/authHelper.js
+++ src/utils/authHelper.js
@@ -18,14 +18,14 @@ export const setCookies = (res, accessToken, refreshToken) => {
   res.cookie("_reflex_accessToken", accessToken, {
     httpOnly: true,
     sameSite: "strict",
-    secure: process.env.NODE_ENV === "production",
+    secure: process.env.NODE_ENV !== "production",
     maxAge: 30 * 60 * 60 * 1000,
   });
 
   res.cookie("_reflex_refreshToken", refreshToken, {
     httpOnly: true,
     sameSite: "strict",
-    secure: process.env.NODE_ENV === "production",
+    secure: process.env.NODE_ENV !== "production",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
